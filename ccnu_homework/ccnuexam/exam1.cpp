7884a3e084bb8738160dd64a953538d01aec49b9
Humbertzhang <504490160@qq.com>
2018-01-02 20:36:24 +0800

    CCNU Exam

---
+++ ccnu_homework/ccnuexam/exam1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void InsertSort(int *a, int len);
+//二分查找
+int Find(int * sorted, int len, int start, int end, int obj);
+
+int main() {
+    //第一位为哨兵数据
+    int tableT[16] = {0, 119, 120, 100, 95, 89, 88, 56, 87, 45, 31, 12, 10, 8, 5 ,1};
+    InsertSort(tableT, 15);
+    //int sorted[15] = {0};
+    //Msort(tableT, sorted, 0, 14);
+    for(int i = 1; i <= 15; i++){
+        cout << tableT[i] << " ";
+    }
+    cout << endl;
+    int obj = 56;
+    int sorted[] = {120, 119, 100, 95, 89, 88, 87, 56, 45, 31, 12, 10, 8, 5, 1};
+    int index = Find(sorted, 15, 0, 14, 56);
+    cout << "index:" << index <<endl;
+}
+
+void InsertSort(int *a, int len)
+{
+    int i, j;
+    for(i = 2; i <= len; i++){
+        if(a[i] > a[i-1]){
+            a[0] = a[i];
+            for(j = i-1; a[0] > a[j]; --j){
+                a[j+1] = a[j];
+            }
+            a[j+1] = a[0];
+        }
+    }
+}
+
+int Find(int * sorted, int len, int start, int end, int obj)
+{
+    int mid = len/2;
+    if(sorted[mid] == obj){
+        return mid;
+    }
+    else if(sorted[mid] > obj){
+        return Find(sorted, mid - start, start, mid, obj);
+    }
+    else if(sorted[mid] < obj){
+        return Find(sorted, end - mid, mid, end, obj);
+    }
+}
---
+++ ccnu_homework/ccnuexam/exam2.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+using namespace std;
+
+typedef char TElemType;
+
+typedef struct BiTNode{
+    TElemType data;
+    struct BiTNode * lchild, *rchild;
+}BiTNode, *BiTree;
+
+//构建二叉树
+void BuildTree(BiTree &T);
+
+//先序遍历树
+void TraTree(BiTree T);
+
+//检查深度，配合MAXDEPTH 与 MINDEPTH来记录最大与最小深度
+void CheckDepth(BiTree T, int depth);
+
+//判断是否平衡
+void CheckBalance();
+
+//用于检验二叉树是否平衡的全局变量
+int MINDEEP;
+int MAXDEEP;
+
+// ABD##EG##H##C#F##
+
+int main()
+{
+    BiTree t;
+    cout << "输入字符串:" <<endl;
+    BuildTree(t);
+    TraTree(t);
+    int depth = 1;
+    MAXDEEP = -1;
+    MINDEEP = 65535;
+    CheckDepth(t, depth);
+    cout << "Mindepth:" << MINDEEP << endl;
+    cout << "Maxdepth:" << MAXDEEP << endl;
+    CheckBalance();
+    return 0;
+}
+
+void TraTree(BiTree T)
+{
+    if(T){
+        cout << T->data << " "<<endl;
+    }
+    if(T->lchild){
+        TraTree(T->lchild);
+    }
+    if(T->rchild){
+        TraTree(T->rchild);
+    }
+}
+
+void BuildTree(BiTree &T)
+{
+    char ch;
+    cin >> ch;
+    if(ch == '#') {
+        T == NULL;
+    }else{
+        T = new BiTNode;
+        T->data = ch;
+        BuildTree(T->lchild);
+        BuildTree(T->rchild);
+    }
+}
+
+void CheckDepth(BiTree T, int depth)
+{
+     if(T->lchild){
+         CheckDepth(T->lchild, depth+1);
+     }
+     if(T->rchild){
+        CheckDepth(T->rchild, depth+1);
+     }
+     if(T->rchild == NULL && T->rchild == NULL){
+        if(depth > MAXDEEP){
+            MAXDEEP = depth;
+        }
+        if(depth < MINDEEP){
+            MINDEEP = depth;
+        }
+     }
+}
+
+void CheckBalance()
+{
+    if(MAXDEEP-MINDEEP <= 1){
+        cout << "平衡" <<endl;
+    }else{
+        cout << "不平衡" <<endl;
+    }
+}
